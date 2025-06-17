efd1488243cf84f77273b40c5b0218842429ae5a
VladZF <vlad2003587@gmail.com>
2025-06-17 20:49:19 +0300

    сделаны кнопки авторизации

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
+import { signIn, signOut } from "@/auth";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <form
+      action={async () => {
+        "use server"
+        await signIn("google")
+      }}
+    >
+      <button type="submit">Signin with Google</button>
+    </form>
+    <form
+      action={async () => {
+        "use server"
+        await signOut()
+      }}
+    >
+      <button type="submit">Sign Out</button>
+    </form>
         <Image
           className="dark:invert"
           src="/next.svg"
