dad5709ee5b230f0d32a49fb330dde64fc0ab583
Valeria Lapshina <lapshina_valeria@mail.ru>
2025-06-18 21:25:45 +0300

    Создана страница опросов у админа

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,22 @@
+﻿import {auth} from "@/auth";
+import {redirect} from "next/navigation";
+import {UserRole} from "@/generated/prisma";
+const axios = require('axios').default;
+
+export default async function AllSurveys() {
+    const session = await auth();
+    const role = session?.user.role
+
+    try {
+        const response = await axios.get('/api/surveys');
+        console.log(response);
+    } catch (error) {
+        console.error(error);
+    }
+
+    if (!role || role !== UserRole.SURVEY_CREATOR) {
+        redirect("/");
+    }
+
+    return;
+}
